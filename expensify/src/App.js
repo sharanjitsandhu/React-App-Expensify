43b6aeb4e74a4531e9d1bcb41537512b482bb272
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-05-28 21:24:15 -0700

    Set up a 404 page using Switch

--- expensify/src/App.js
+++ expensify/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ExpenseDashBoard from './components/ExpenseDashboard.js';
 import AddExpense from './components/AddExpense.js';
 import EditExpense from './components/EditExpense.js';
 import Help from './components/Help.js';
+import NotFound from './components/NotFound.js';
 
 import './App.css';
 
@@ -11,10 +12,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Expensify App</header>
-      <Route exact path="/" component={ExpenseDashBoard} />
-      <Route path="/add-expense" component={AddExpense} />
-      <Route path="/edit-expense" component={EditExpense} />
-      <Route path="/help" component={Help} />
+      <Switch>
+        <Route exact path="/" component={ExpenseDashBoard} />
+        <Route path="/add-expense" component={AddExpense} />
+        <Route path="/edit-expense" component={EditExpense} />
+        <Route path="/help" component={Help} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
